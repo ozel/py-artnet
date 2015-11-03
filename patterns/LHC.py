1055f8a4f26b37c8288c5dcfa066256a8a9ca49c
Oliver Keller <oliver.michael.keller@cern.ch>
2015-11-03 19:46:01 +0100

    coverage of SPS and LHC
    the LED stripes are spread across the machine lines which requires some
    remapping voodoo

--- patterns/LHC.py
+++ patterns/LHC.py
@@ -10,23 +10,37 @@ import sys
 import os
 import serial
 import liblo #for OSC messaging
+import random
+
+CW = 0
+CCW = 1
 
 BOT_BEGIN = 4
 BOT_END   = 37
 DUO_BEGIN = 38
 DUO_END   = 68
 LIN_BEGIN = 0
-LIN_END   = 168
+# #LIN_END   = 168+24+1
 SPS_BEGIN = 13
-SPS_END   = 48
+#SPS_END   = 48
 LHC_BEGIN = 4
-LHC_END   = 167
+# LHC_END   = 167
 
 bot_pat = [(0,0,127),BLUE,(127,0,0),RED]
 duo_pat = [BLACK,BLACK,(107,0,0),RED,(127,0,0)]
-lin_pat = [RED,RED,RED]
+lin_pat = [(127,0,0),RED,(127,0,0)]
 sps_pat = [(100,100,100),WHITE,(100,100,100)]
 lhc_pat = [(127,127,0),YELLOW,(127,127,0)]
+
+LIN_SIZE = 168 + 24 + 1 + 1
+lin_conf = [ LIN_SIZE, [0, [0,168]], [2, [0, 24]]  ]
+
+SPS_SIZE = 48 - 13 + 1
+sps_conf = [ SPS_SIZE, [4, [13, 48]] ]
+
+LHC_SIZE = 167 - 27 + 1 + 2 + 12
+lhc_conf = [ LHC_SIZE, [3, [4, 5]], [4, [54, 65]], [3, [27, 167]]]
+
 global_i = 0
 
 def repeat(times, func, *args, **kwargs):
@@ -38,17 +52,25 @@ def parabola (x):
 
 class Bunch():
 
-    def __init__(self, graphics, pattern, min_x, max_x, acc, y, mode=0):
+    def __init__(self, graphics, config, pattern, acc, mode=0, direction=0):
         self.graphics = graphics
+        self.config = config
         self.pattern = pattern
-        self.x = min_x+1
-        self.min_x = min_x
-        self.max_x = max_x
-        self.range = max_x - min_x
-        self.y = y
+        first_section = config[1]
+        #self.y = first_section[0]
+        #self.x = first_section[1][0]
         self.acc = acc
-        self.alive = True
+        self.cycles = 0
         self.mode = mode # lin, sps, lhc  
+        self.section_base_x = config[1][1][0]
+        self.max_x = config[0]
+        self.dir=direction
+        if self.dir == 0:
+            # clockwise
+            self.x = 0
+        else:
+            # counter clockwise
+            self.x = self.max_x
 
 #    def __del__(self):
         #print('bunch deleted')
@@ -62,52 +84,59 @@ class Bunch():
         # self.acc = 0
         # self.alive = False
 
-    def isAlive(self):    
-        return self.alive
+    def cycles(self):    
+        return self.cycles
 
     def travel(self):
         global global_i
 
         #self.x = self.min_x + 1*(global_i%self.range) + (self.acc*(global_i%self.range)*(global_i%self.range))/2
-        if global_i % 1 == 0:
-            self.x =  self.min_x + ((self.x+1) * self.acc)
+        if self.acc > 1.0 and global_i % 2 == 0:
+            self.x = (self.x*self.acc) + 0.35
+        elif self.dir == 1:
+            self.x -=1
+        else:
+            self.x +=1
+
+        # if self.x <= 0:  
+        #     self.x = 1
+        
         #print(self.x)
-        if self.x > self.max_x:
-            #self.x = self.min_x
-            self.alive = False
+        if self.x > self.max_x and self.dir == 0:
+            self.x = 0
+            self.cycles += 1
+        elif self.x < 0 and self.dir == 1:
+            self.x = self.max_x
+            self.cycles += 1
 
     def draw(self, cur_x):
         global global_i
-        #i = 0
-        
-        if self.mode == 3:
-            offset = LHC_END
-            sign = +1
-        else:
-            offset = 0
-            sign = -1
-         
         #for color in self.pattern:
-        color = self.pattern[(int(-global_i)%len(self.pattern))]
-        self.graphics.drawPixel(offset-(sign*self.x), self.y, color)
-        #self.graphics.drawPixel(offset-(sign*self.x), self.y, self.pattern[1])
-        #if self.mode >= 2 or (self.mode == 1 and self.x > SPS_END):
-        #    self.graphics.drawPixel(offset-(sign*self.x-1),self.y, self.pattern[2])
-        #    self.graphics.drawPixel(offset-(sign*self.x+1),self.y, self.pattern[0])
-
-        
-        # if self.x in list([((x-cur_x)*(x-cur_x)) for x in range(self.min_x, self.max_x)]):
-        #     self.graphics.drawPixel(self.y, self.x, RED)
-        # else:
-        #     self.graphics.drawPixel(self.y, self.x, BLACK)
-
-        # if i-global_i in [(x*x) for x  in xrange(200)]: #in list([((x-i)*(x-i)) for x in xrange(LIN_END)]) :
-        #     self.graphics.drawPixel(self.y, pixel, RED) #pattern[(pixel-self.i)%len(pattern)])
-        # else:
-        #     self.graphics.drawPixel(self.y, pixel, BLACK)
-        #i += 
-
-
+        #color = self.pattern[(int(-global_i)%len(self.pattern))]
+        #self.graphics.drawPixel(offset-(sign*self.x), self.y, color)
+        x = int(round(self.x))
+        min_x = self.config[1][1][0]
+        max_x = self.config[1][1][1]
+        range_x = max_x - min_x
+        self.section_base_x = 0
+        for index,section in enumerate(self.config[1:]):
+            print("section", index)
+            min_x = section[1][0]
+            max_x = section[1][1]
+            range_x = (max_x - min_x) + 1
+            print(min_x,max_x,self.section_base_x,range_x)
+            #print(self.x,self.section_base_x,  self.section_base_x+range_x)
+            y = section[0]
+            if x in range(self.section_base_x, self.section_base_x+range_x):
+                #print("in range")
+                self.graphics.drawPixel(x-self.section_base_x+min_x, y, self.pattern[1])
+                print(x,y)
+                if self.mode >= 1 and (self.x+1) > self.mode:
+                    if self.x - 1 > self.section_base_x - min_x:
+                        self.graphics.drawPixel(x-self.section_base_x+min_x-1,y, self.pattern[2])
+                    if self.x + 1 < self.section_base_x + range_x:
+                        self.graphics.drawPixel(x-self.section_base_x+min_x+1,y, self.pattern[0])
+            self.section_base_x += range_x
 
 class LHC(object):
     def __init__(self):
@@ -175,21 +204,22 @@ class LHC(object):
             if self.debug:
                 print(self.duo_pos)
 
-    def updateLINAC(self, pattern):
+    def updateLINAC(self, config, pattern):
         global global_i
         if self.duo_done :
             del_bunches = list()
             for index, bnch in enumerate(self.lin_bunches):
-                if bnch.isAlive():
+                if bnch.cycles == 0:
                     #print('ALIVE')
                     bnch.draw(self.lin_pos)    
                     bnch.travel()
                 else:
+                    #print("creating SPS bunches")
                     self.lin_done = True
-                    new_bunch = Bunch(self.graphics, sps_pat, SPS_BEGIN,SPS_END,1,4,2)
+                    new_bunch = Bunch(self.graphics, sps_conf, sps_pat,1.0,1)
                     self.sps_bunches.append(new_bunch)
-                    new_bunch = Bunch(self.graphics, sps_pat, SPS_BEGIN,SPS_END,1,4,2)
-                    self.sps_bunches.append(new_bunch)
+                    # new_bunch = Bunch(self.graphics, sps_conf, sps_pat,1,1)
+                    # self.sps_bunches.append(new_bunch)
                     del_bunches.append(index)
 
             #print ("before bunches", len(self.bunches))                
@@ -199,9 +229,9 @@ class LHC(object):
             
             #print ("after bunches", len(self.bunches))
 
-            if  global_i % 8 == 0:
+            if  global_i % 8 == 0: #accelerate protons in linac 
             #if global_i == 0:
-                new_bunch = Bunch(self.graphics, pattern, LIN_BEGIN,LIN_END,1.2,0)
+                new_bunch = Bunch(self.graphics, config, pattern,1.1,168)
                 self.lin_bunches.append(new_bunch)
                 liblo.send(self.osc_target, "/collision", ('T',1))
                 #print('NEW BUNCH', global_i % 10)
@@ -214,60 +244,42 @@ class LHC(object):
             if self.debug:
                 print(self.lin_pos)
     
-    def updateSPS(self,pattern):
+    def updateSPS(self, config, pattern):
         global global_i
         if self.lin_done : 
-            # self.animateSrtripe(sps_pat, self.sps_pos,SPS_BEGIN,SPS_END,2,3) 
-            # self.sps_pos += 1
-            # if self.sps_pos >= SPS_END:
-            #     self.sps_done = True
-            # if self.debug:
-            #     print(self.sps_pos)       
             del_bunches = list()
             for index, bnch in enumerate(self.sps_bunches):
-                if bnch.isAlive():
+                if bnch.cycles == 0:
                     #print('ALIVE')
                     bnch.draw(self.sps_pos)    
                     bnch.travel()
                 else:
                     self.sps_done = True
-                    new_bunch = Bunch(self.graphics, lhc_pat, LHC_BEGIN,LHC_END,1,3,2)
-                    self.lhc_bunches.append(new_bunch)
-                    new_bunch = Bunch(self.graphics, lhc_pat, LHC_BEGIN,LHC_END,1,3,3)
-                    self.lhc_bunches.append(new_bunch)
-                    new_bunch = Bunch(self.graphics, lhc_pat, LHC_BEGIN,LHC_END,1,3,2)
-                    self.lhc_bunches.append(new_bunch)
-                    new_bunch = Bunch(self.graphics, lhc_pat, LHC_BEGIN,LHC_END,1,3,3)
-                    self.lhc_bunches.append(new_bunch)
+                    if  global_i % 3 == 0:
+                        new_bunch = Bunch(self.graphics, lhc_conf, lhc_pat,1.0,1,CW)
+                        self.lhc_bunches.append(new_bunch)
+                        new_bunch = Bunch(self.graphics, lhc_conf, lhc_pat,1.0,1,CCW)
+                        self.lhc_bunches.append(new_bunch)
+                    # new_bunch = Bunch(self.graphics, lhc_conf, lhc_pat,1,0)
+                    # self.lhc_bunches.append(new_bunch)
+                    # new_bunch = Bunch(self.graphics, lhc_conf, lhc_pat,1,0)
+                    # self.lhc_bunches.append(new_bunch)
                     del_bunches.append(index)
 
             #print ("before bunches", len(self.bunches))                
 
             for index in sorted(del_bunches, reverse=True):
                 del self.sps_bunches[index]
-            
-            #print ("after bunches", len(self.bunches))
-
-            # if  global_i % 10 == 0:
-            # #if global_i == 0:
-            #     new_bunch = Bunch(self.graphics, pattern, SPS_BEGIN,SPS_END,1.2,3)
-            #     self.sps_bunches.append(new_bunch)
-                #print('NEW BUNCH', global_i % 10)
-
-            # self.lin_pos += 1
-            # if self.lin_pos >= LIN_END:
-            #     self.lin_done = True
-            #     self.lin_pos = 0
 
             if self.debug:
                 print(self.sps_pos)        
 
-    def updateLHC(self,lr):
+    def updateLHC(self, config, pattern):
         global global_i
         if self.sps_done : 
             del_bunches = list()
             for index, bnch in enumerate(self.lhc_bunches):
-                if bnch.isAlive():
+                if bnch.cycles == 0:
                     #print('ALIVE')
                     bnch.draw(self.lhc_pos)    
                     bnch.travel()
@@ -279,7 +291,7 @@ class LHC(object):
                 del self.lhc_bunches[index]
             
             #print(self.lhc_pos)  
-            if self.lhc_pos > 1* (LHC_END-LHC_BEGIN) :
+            if self.lhc_pos > 5* (LHC_SIZE) :
                 #sys.exit(2)
                 return 0
             else:
@@ -298,14 +310,13 @@ class LHC(object):
             #os.system('read -s -n 1')
             self.restart = False
         self.graphics.fill(BLACK)
-        # self.bot_done = True
-        # self.duo_done = True
+        self.bot_done = True
+        self.duo_done = True
         self.updateBottle()
         self.updateDuoplasmatron()
-        self.updateLINAC(lin_pat)
-        #self.updateSPS(sps_pat)
-        #self.updateLHC(0)
-        if not self.updateLHC(1):
+        self.updateLINAC(lin_conf, lin_pat)
+        self.updateSPS(sps_conf, sps_pat)
+        if not self.updateLHC(lhc_conf, lhc_pat):
             # reset
             print('LHC LED strip animation ended')
             self.graphics.fill(BLACK)
